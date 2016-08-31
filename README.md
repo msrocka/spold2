b8d190ef3e0c53b6f45c8b60dd662d80cf322b1f
msrocka <michael.srocka@gmail.com>
2016-08-31 10:14:18 +0200

    added EachFile function

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Install Go and run `go get`:
 go get github.com/msrocka/spold2
 ```
 
-### Reading an EcoSpold 2 file
+### Reading a single EcoSpold 2 file
 
 ```go
 package main
@@ -23,11 +23,30 @@ import (
 )
 
 func main() {
-	spold, err := spold2.ReadFile("path/to/file")
+	spold, err := spold2.ReadFile("path/to/file.spold")
 	if err != nil {
 		// handle error
 		return
 	}
 	fmt.Println(spold.DataSet.Description.Name)
+}
+```
+
+### Processing each EcoSpold 2 file of a folder
+
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/msrocka/spold2"
+)
+
+func main() {
+	dir := "folder/with/datasets"
+	err := spold2.EachFile(dir, func(ds *spold2.ActivityDataSet) error {
+		fmt.Println(ds.Description.Name)
+		return nil
+	})
 }
 ```
--- io.go
+++ io.go
@@ -3,6 +3,7 @@ package spold2
 import (
 	"encoding/xml"
 	"io/ioutil"
+	"path/filepath"
 )
 
 // ReadFile reads an EcoSpold 2 file.
@@ -14,4 +15,24 @@ func ReadFile(fileName string) (*EcoSpold, error) {
 	spold := &EcoSpold{}
 	err = xml.Unmarshal(data, spold)
 	return spold, err
+}
+
+// EachFile parses each file in the given folder and calls the given handler
+// with the data sets from respective files.
+func EachFile(folder string, fn func(*ActivityDataSet) error) error {
+	files, err := ioutil.ReadDir(folder)
+	if err != nil {
+		return err
+	}
+	for _, info := range files {
+		path := filepath.Join(folder, info.Name())
+		spold, err := ReadFile(path)
+		if err != nil {
+			return err
+		}
+		if err = fn(spold.GetDataSet()); err != nil {
+			return err
+		}
+	}
+	return nil
 }
